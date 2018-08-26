730b73df7652bbe89d1a09b629216ebeb5fb29d2
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-25 20:06:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let i = 0; i < array.length; i++) {
+    // LOG: Welcome Ada! You are employee #1.
+    console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+  }
+
+  return array;
+}
+
+function maybeTrue () {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+
+  while (maybeTrue()) {
+    count++;
+  }
+  // => "You got 3 tails in a row!"
+  return `You got ${count} tails in a row!`;
+}
