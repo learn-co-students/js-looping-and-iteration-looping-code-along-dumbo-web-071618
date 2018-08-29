bfe9c320656d172a9c853b7c8d389f9d953972fe
Nkosi Enciso Givhan <codeatblack@gmail.com>
2018-08-29 11:47:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function tailsNeverFails(){
+  let bool = true;
+  let count = 0;
+  while (bool){
+    if (Math.random() <= .5) {
+      count++
+    } else {
+      return `You got ${count} tails in a row!"`;
+    }
+  }
+}
