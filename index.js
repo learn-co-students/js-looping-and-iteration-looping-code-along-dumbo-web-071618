82e7edf27d8a14b37f9193c97da344eba8dcb846
ma539 <ma539@cornell.edu>
2018-08-29 15:03:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges (employees) {
+  for (let employee=0; employee < employees.length; employee++) {
+    console.log(`Welcome ${employees[employee]}! You are employee #${employee+1}.`)
+  }
+  return employees
+};
+
+
+function tailsNeverFails () {
+  function Tails() {
+    return Math.random() >= 0.5
+  }
+  tails = 0
+  while (Tails()) {
+    tails = tails + 1
+  }
+  return `You got ${tails} tails in a row!`
+};
